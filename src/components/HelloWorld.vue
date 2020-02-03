c37292e4d60d1c2a642142f6fbec4fa03e8f4884
yimialmonte <yelmialmonte@gmail.com>
2020-02-03 13:47:56 -0400

    fake post vuejs

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="form-check form-check-inline">
+        <label class="form-check-label" for></label>
+        <input
+          type="checkbox"
+          class="form-check-input"
+          name="typePost"
+          value="even"
+          v-model="postType"
+        /> Even
+        <input
+          type="checkbox"
+          class="form-check-input"
+          name="typePost"
+          value="odd"
+          v-model="postType"
+        /> Odd
+      </div>
+      <table class="table" v-if="postType.length > 0">
+        <thead>
+          <tr>
+            <th scope="col" v-show="postType.length === 2">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="post in postToShow"
+            :key="post.id"
+            :style="post.id % 2 === 0 ? 'background-color: white' : 'background-color: #f8f9fa'"
+            @click="clickPost(post.id)"
+            class="postRow"
+          >
+            <th scope="row" v-show="postType.length === 2">{{ post.id }}</th>
+            <td>{{ post.title }}</td>
+            <td>{{ post.body }}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div v-else>
+        <p>Please select one checkbox</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import dataSource from "../../src/data.json";
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App",
+      posts: dataSource,
+      postType: []
+    };
+  },
+  computed: {
+    postToShow: function() {
+      if (this.postType.length === 0) return [];
+      let postShow = [];
+      let even = [];
+      let odd = [];
+      if (this.postType.findIndex(e => e === "even") !== -1) {
+        even = dataSource.filter(function(post) {
+          return post.id % 2 === 0;
+        });
+      }
+
+      if (this.postType.findIndex(e => e === "odd") !== -1) {
+        odd = dataSource.filter(function(post) {
+          return post.id % 2 !== 0;
+        });
+      }
+
+      return [...odd, ...even].sort(function(a, b) {
+        return a.id - b.id;
+      });
+    }
+  },
+  methods: {
+    clickPost: function(postId) {
+      let post = this.posts.find(e => e.id === postId);
+      post.title = post.title.substring(0, 20) + "...";
+      post.body = post.body.substring(0, 50) + "...";
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.postRow {
+  cursor: pointer;
+}
+</style>
